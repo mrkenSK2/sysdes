53e17cb2cea3c802a149a38c9466e5e28d8fa158
root <root@DESKTOP-QS2V39J.localdomain>
2022-11-08 22:18:38 +0900

    sqlにパスワードなど追加

--- docker/db/sql/01_create_tables.sql
+++ docker/db/sql/01_create_tables.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS `tasks`;
 
 CREATE TABLE `tasks` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL UNIQUE,
+    `password`   binary(32) NOT NULL,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `title` varchar(50) NOT NULL,
     `is_done` boolean NOT NULL DEFAULT b'0',
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
