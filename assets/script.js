3c69ff4a4e90d545ae395e47d54c9939092978ba
root <root@DESKTOP-QS2V39J.localdomain>
2022-11-09 15:31:18 +0900

    パスワード再入力チェック以外

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,19 @@ const confirm_update = (id) => {
         return false;
     }
 }
+
+function pushEyeButton() {
+    var passwdElm = document.getElementById("password");
+    var re_enterPasswdElm = document.getElementById("re_enter_password");
+    var eyeBtn = document.getElementById("eyeButton");
+    if (passwdElm.type === "text") {
+        passwdElm.type = "password";
+        re_enterPasswdElm.type = "password";
+        eyeBtn.className = "fa fa-eye";
+    } else {
+        passwdElm.type = "text";
+        re_enterPasswdElm.type = "text";
+        eyeBtn.className = "fa fa-eye-slash";
+    }
+}
+  
--- service/user.go
+++ service/user.go
@@ -24,15 +24,29 @@ func RegisterUser(ctx *gin.Context) {
     // フォームデータの受け取り
     username := ctx.PostForm("username")
     password := ctx.PostForm("password")
-    if username == "" || password == "" {
-        Error(http.StatusBadRequest, "Empty parameter")(ctx)
-        return
+    switch {
+        case username == "":
+            ctx.HTML(http.StatusBadRequest, "new_user_form.html", gin.H{"Title": "Register user", "Error": "Usernane is not provided", "Username": username})
+        case password == "":
+            ctx.HTML(http.StatusBadRequest, "new_user_form.html", gin.H{"Title": "Register user", "Error": "Password is not provided", "Password": password})
     }
     
     // DB 接続
     db, err := database.GetConnection()
     if err != nil {
         Error(http.StatusInternalServerError, err.Error())(ctx)
+        return
+    }
+
+    // 重複チェック
+    var duplicate int
+    err = db.Get(&duplicate, "SELECT COUNT(*) FROM users WHERE name=?", username)
+    if err != nil {
+        Error(http.StatusInternalServerError, err.Error())(ctx)
+        return
+    }
+    if duplicate > 0 {
+        ctx.HTML(http.StatusBadRequest, "new_user_form.html", gin.H{"Title": "Register user", "Error": "Username is already taken", "Username": username, "Password": password})
         return
     }
  
